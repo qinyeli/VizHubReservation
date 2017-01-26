97a4f9bc8bd91c1566fe36e567d7810613b69cc0
Qinye Li <qinyeli@umich.edu>
2017-01-25 21:13:40 -0500

    Reservation successful; but time setting not working

--- reserveVizHub.py
+++ reserveVizHub.py
@@ -1,44 +1,53 @@
 #!/usr/bin/env python3
 
 import requests
+import json
+import getpass
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+uuid = {
+	1: 'svdkdsflk', # 10
+	2: 'lkklmrkja' # 10
+}
+
+def getTime():
+	datetime.strftime("%Y %m %d %H %M %S")
+	now = datetime.now()
 
 def main():
-
 	print("Running Qinye's VizHub reserving script...")
+	# login = input('User: ')
+	# password = getpass.getpass()
 
-	# r = requests.get('https://weblogin.umich.edu/')
-	# #print(r.status_code, r.text)
+	start, end = getTime()
+	print('Reserving VizHub from ' + start + ' to ' + end)
+
+	# session = requests.Session()
+
+	# # Login
+	# response = requests.get('http://apps.engin.umich.edu/collab-reserve/')
+	# cookies = dict(response.cookies)
+	# form = {
+	# 	'ref': 'https://apps.engin.umich.edu/collab-reserve/',
+	# 	'service': 'cosign-apps.engin',
+	# 	'required': '',
+	# 	'login': login,
+	# 	'password': password
+	# }
+	# response = session.post('https://weblogin.umich.edu/cosign-bin/cosign.cgi',\
+	# 	data=form, cookies=cookies)
+
+	# response = session.get('https://apps.engin.umich.edu/collab-reserve/reservatron.php?action=reserve&uuid=' + uuid[1]
+	# 	+ '&start=' + start + '&end=' + end)
+
+	# # The response is going to be something like {'id': 12345}
+	# id = json.loads(respone.text)['id']
+	# response = session.get('https://apps.engin.umich.edu/collab-reserve/reservatron.php?action=status&id=' + id)
 	
-	# soup = BeautifulSoup(r.text, 'html.parser')
-	# print(soup.title)
-	# print(soup.find_all('form'))
-	
-	s = requests.Session()
-	form = {
-		'ref': '',
-		'service': '',
-		'required': '',
-		'login': 'login',
-		'password': 'password'
-	}
-	r = s.post("https://weblogin.umich.edu/cosign-bin/cosign.cgi", data=form)
-	# r = requests.get('http://apps.engin.umich.edu/collab-reserve/')
-	print(r.status_code)
-	print(r.text)
-
-	# s.cookies
-
-	# connection = http.client.HTTPConnection('apps.engin.umich.edu')
-	# connection.request('GET', '/collab-reserve/')
-	# response = connection.getresponse()
-	# content = response.read().decode('utf-8')
-	# print(content)
-
-	# # Log in needed
-	# if response.status == 302:
-	# 	print (response.reason)
-	# 	print("lalal")
+	# # The response is going to be
+	# # either {"message":"Queued"} or {"message":"Created"}
+	# print(json.loads(response.text)['message'])
 
 if __name__ == '__main__':
 	main()
