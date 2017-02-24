8ae69687ba2c274ad1ad96826b135fbf56bbbccc
Qinye Li <qinyeli@umich.edu>
2017-02-24 18:20:42 -0500

    Finished! Reserved 4 days of Vizhub with the script!

--- reserveVizHub.py
+++ reserveVizHub.py
@@ -3,51 +3,112 @@
 import requests
 import json
 import getpass
-from bs4 import BeautifulSoup
-from datetime import datetime
+import time
 
 uuid = {
 	1: 'svdkdsflk', # 10
-	2: 'lkklmrkja' # 10
+	2: 'lkklmrkja', # 10
+	3: 'ioqyDKJkbj', # 5
+	4: 'ZhCbGLdzeC', # 5
+	5: 'SriULTysLk', # 5
+	6: 'FhQvgIbxoN', # 5
+	7: 'XZCDatSVIN', # 10
+	8: 'OvzyGxMaEp', # 10
+	9: 'ALKEamOYNU', # 5
+	10: 'COxXAIIzIT', # 5
+	11: 'pafUhTdvvk', # 4
+	12: 'NmauMgqblC', # 4
+	13: 'EFuHkeNKrZ' # 4
 }
 
+# epochTime() returns the epoch time for begin time and end time
+def epochTime(day_from_today, hour_begin, hour_end):
+	# Ref: https://docs.python.org/3/library/time.html
+	# Note: tm_isdst means daylight summer time
+
+	# strftime converts time.struct_time into string
+	year = time.strftime('%Y')
+	day = int(time.strftime('%j')) # day of year
+	day += int(day_from_today)
+
+	# strptime converts string into time.struct_time
+	time_begin = time.strptime(year + ' ' + str(day) + ' ' + hour_begin, '%Y %j %H')
+	time_end = time.strptime(year + ' ' + str(day) + ' ' + hour_end, '%Y %j %H')
+
+	print('Reserving VizHub for ' + time.strftime('%b. %d %a %H:%M ~ ', time_begin)
+		+ time.strftime('%H:%M', time_end))
+
+	# mktime converts (local) time.struct_time into epoch time
+	epoch_begin = str(int(time.mktime(time_begin)))
+	epoch_end = str(int(time.mktime(time_end)))
+
+	return epoch_begin, epoch_end
+
 def getTime():
-	datetime.strftime("%Y %m %d %H %M %S")
-	now = datetime.now()
+	days = input('Input the days (0 for today, 1 for tomorrow and so on): ').split()
+	assert (len(days) > 0)
+
+	hours = input('Input the begin time and the end time (in 24-hour clock): ').split()
+	assert(len(hours) == 2)
+
+	return days, hours[0], hours[1]
+
+def login():
+	# Ref: http://docs.python-requests.org/
+	login = input('User: ')
+	if login == '':
+		login = 'qinyeli'
+	password = getpass.getpass()
+
+	session = requests.Session()
+
+	response = requests.get('http://apps.engin.umich.edu/collab-reserve/')
+	cookies = dict(response.cookies)
+	form = {
+		'ref': 'https://apps.engin.umich.edu/collab-reserve/',
+		'service': 'cosign-apps.engin',
+		'required': '',
+		'login': login,
+		'password': password
+	}
+	response = session.post('https://weblogin.umich.edu/cosign-bin/cosign.cgi',\
+		data=form, cookies=cookies)
+
+	# print(response.status_code)
+	# print(response.headers)
+	# print(response.text)
+
+	if response.text[0 : len('<!-- login_error.html -->')] == '<!-- login_error.html -->':
+		print("Password or Account Name Incorrect. Is [caps lock] on?")
+		return False, session
+	else:
+		return True, session
+
 
 def main():
-	print("Running Qinye's VizHub reserving script...")
-	# login = input('User: ')
-	# password = getpass.getpass()
+	ok = False
+	while not ok:
+		ok, session = login()
 
-	start, end = getTime()
-	print('Reserving VizHub from ' + start + ' to ' + end)
+	vizhubID = int(input('Input the VizHub you want to reserve: '))
+	assert(vizhubID != '')
 
-	# session = requests.Session()
+	days, hour_begin, hour_end = getTime()
 
-	# # Login
-	# response = requests.get('http://apps.engin.umich.edu/collab-reserve/')
-	# cookies = dict(response.cookies)
-	# form = {
-	# 	'ref': 'https://apps.engin.umich.edu/collab-reserve/',
-	# 	'service': 'cosign-apps.engin',
-	# 	'required': '',
-	# 	'login': login,
-	# 	'password': password
-	# }
-	# response = session.post('https://weblogin.umich.edu/cosign-bin/cosign.cgi',\
-	# 	data=form, cookies=cookies)
+	for day_from_today in days:
+		epoch_begin, epoch_end = epochTime(day_from_today, hour_begin, hour_end)
 
-	# response = session.get('https://apps.engin.umich.edu/collab-reserve/reservatron.php?action=reserve&uuid=' + uuid[1]
-	# 	+ '&start=' + start + '&end=' + end)
+		response = session.get('https://apps.engin.umich.edu/collab-reserve/reservatron.php?action=reserve&uuid='
+			+ uuid[vizhubID] + '&start=' + epoch_begin + '&end=' + epoch_end)
 
-	# # The response is going to be something like {'id': 12345}
-	# id = json.loads(respone.text)['id']
-	# response = session.get('https://apps.engin.umich.edu/collab-reserve/reservatron.php?action=status&id=' + id)
-	
-	# # The response is going to be
-	# # either {"message":"Queued"} or {"message":"Created"}
-	# print(json.loads(response.text)['message'])
+		id = json.loads(response.text)['id']
+		status = 'Queued' # status will be Queued, Processing, Created, or Failed
+		while status == 'Queued' or status == 'Processing':
+			time.sleep(1)
+			response = session.get('https://apps.engin.umich.edu/collab-reserve/reservatron.php?action=status&id=' + str(id))
+			status = json.loads(response.text)['message']
+			print(status)
 
 if __name__ == '__main__':
+	print("Running Qinye's VizHub reserving script...")
 	main()
